e754ce9c33332ab5dfc2a694ace7fb2039bbab2f
Drazen Nikolic <extncd@audatex.com>
2018-05-30 08:55:38 +0200

    Lombok refactoring.

--- src/main/java/com/example/reactivespring/model/Employee.java
+++ src/main/java/com/example/reactivespring/model/Employee.java
@@ -1,6 +1,10 @@
 package com.example.reactivespring.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class Employee {
   private Integer id;
   private String firstName;
@@ -8,63 +12,4 @@ public class Employee {
   private Integer age;
   private Gender gender;
 
-  public Employee(Integer id, String firstName, String lastName, Integer age,
-      Gender gender) {
-    this.id = id;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.gender = gender;
-  }
-
-  public Integer getId() {
-    return id;
-  }
-
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public String getFirstName() {
-    return firstName;
-  }
-
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-
-  public String getLastName() {
-    return lastName;
-  }
-
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
-
-  public Integer getAge() {
-    return age;
-  }
-
-  public void setAge(Integer age) {
-    this.age = age;
-  }
-
-  public Gender getGender() {
-    return gender;
-  }
-
-  public void setGender(Gender gender) {
-    this.gender = gender;
-  }
-
-  @Override
-  public String toString() {
-    return "Employee{" +
-        "id=" + id +
-        ", firstName='" + firstName + '\'' +
-        ", lastName='" + lastName + '\'' +
-        ", age=" + age +
-        ", gender=" + gender +
-        '}';
-  }
 }
